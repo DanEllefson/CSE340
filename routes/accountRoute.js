f78cd2c441066754436b91cad7310924df334174
Dan Ellefson <41584162+DanEllefson@users.noreply.github.com>
2024-12-02 18:50:04 -0600

    Week 5: All step 1 done, in the middle of step 2, up to but not including the Account Management View section.

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -19,22 +19,12 @@ router.post(
   utilities.handleErrors(accountController.registerAccount)
 )
 
-// // Process the login attempt
-// router.post(
-//   "/login",
-//   (req, res) => {
-//     res.status(200).send('login process')
-//   }
-// )
-
-// Process the login attempt
+// Process the login request
 router.post(
   "/login",
   regValidate.loginRules(),
   regValidate.checkLoginData,
-  (req, res) => {
-    res.status(200).send('login process')
-  }
+  utilities.handleErrors(accountController.accountLogin)
 )
 
 module.exports = router;
