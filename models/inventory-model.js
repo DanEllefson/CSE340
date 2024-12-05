d9dd2afb81131c2f9e63d1c9d389d6c03f82a673
Dan Ellefson <41584162+DanEllefson@users.noreply.github.com>
2024-12-04 18:56:35 -0600

    Inventory Model complete. No testing.

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -138,4 +138,17 @@ invModel.updateInventory = async function updateInventory(
   }
 }
 
+/* ***************************
+ *  Delete Inventory Item
+ * ************************** */
+invModel.deleteInventoryItem = async function (inv_id) {
+  try {
+    const sql = 'DELETE FROM inventory WHERE inv_id = $1'
+    const data = await pool.query(sql, [inv_id])
+  return data
+  } catch (error) {
+    new Error("Delete Inventory Error")
+  }
+}
+
 module.exports = invModel;
